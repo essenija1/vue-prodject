c00d171201a8e79e8ac99fba33a649b3f300b847
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-12-27 10:00:01 +0300

    Create MyButton.vue

---
+++ components/MyButton.vue
@@ -0,0 +1,55 @@
+<template>
+<button @click="onClick">Clicked {{ counter }} times!</button>
+</template>
+<script>
+export default {
+data () {
+return {
+counter: 0,
+}
+},
+methods: {
+onClick () {
+this.counter++
+},
+mouseOver(){
+     console.log('Hovered on button')
+}
+},
+beforeCreate () {
+console.log('beforeCreate')
+},
+created() {
+    console.log('created')
+},
+beforeMount() {
+  console.log('beforeMount')
+},
+mounted() {
+   console.log('mounted')
+   const btn = this.$el.querySelector('button')
+   if (btn) {
+   btn.addEventListener('mouseover', this.onMouseOver)
+}
+},
+beforeUpdate() {
+     console.log('beforeUpdate');
+},
+update() {
+    console.log('updated')
+},
+
+beforeDestroy()
+{
+   const btn = this.$el.querySelector('button')
+   if (btn) {
+   btn.removeEventListener('mouseover', this.onMouseOver)
+   }
+},
+
+destroyed()
+{
+console.log('destroyed')
+},
+};
+</script>
