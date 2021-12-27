d287c48ef0a42f2fa3e7027cbcb06ed1fce64373
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-12-27 09:55:34 +0300

    Create Home.vue

---
+++ views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <calculator />
+    <hr />
+    <my-button v-if="show"/>
+    <button @click="show=!show">Show/Hide</button>
+    
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import AddPaymentForm from '@/components/AddPaymentForm.vue'
+import Calculator from '@/components/Calculator.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import MyButton from '@/components/MyButton.vue'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Calculator,
+    MyButton,
+    AddPaymentForm
+  },
+  data() {
+    return{
+      show: true
+    }
+  },
+};
+</script>
