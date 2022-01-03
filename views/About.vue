18081fbcbe8b65fb91e15c794cf9968f950caa1c
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-03 18:33:16 +0300

    Update About.vue

--- views/About.vue
+++ views/About.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>about</div>
+  <div>about<button @click="openModal">Open</button></div>
 </template>
 
 <script>
 export default {
-  name: "about",
+  name: "About",
+  methods: {
+    openModal() {
+    this.$modal.show('Auth', {
+      content: 'auth',
+      header: 'Auth From'
+    });
+   },
+  },
 };
 </script>
 <style>
