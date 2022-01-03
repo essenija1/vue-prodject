011941d4750cdb785f365e3d7b6ab3b54396c789
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-03 18:33:52 +0300

    Update Dashboard.vue

--- views/Dashboard.vue
+++ views/Dashboard.vue
@@ -1,73 +1,63 @@
 <template>
   <main>
-    <add-payment-form />
     <payments-display :items="paymentsList" />
-    <pagination
-      :cur="page"
-      :n="n"
-      :length="paymentsList.length"
-      @paginate="changePage"
-    />
+    <pagination :cur="page" :n="n" :length="12" @paginate="changePage" />
+    <button @click="openModal">Add new cost +</button>
   </main>
 </template>
 
 <script>
 import PaymentsDisplay from "../components/PaymentsDisplay.vue";
-import AddPaymentForm from "../components/AddPaymentForm.vue";
 import Pagination from "../components/Pagination.vue";
 import { mapMutations, mapGetters, mapActions } from "vuex";
 export default {
-  name: "dashboard",
-  components: { PaymentsDisplay, AddPaymentForm, Pagination },
+  name: "Dashboard",
+  components: {
+    PaymentsDisplay,
+    Pagination,
+  },
   data() {
     return {
-      page: "",
+      addFormShow: false,
+      settings: {
+        content: "addPaymentForm",
+        header: "Add new cost",
+      },
+      page: 1,
+      n: 3,
     };
   },
   computed: {
-    ...mapGetters({ paymentsList: "getPaymentsList" }),
+    ...mapGetters(["getPaymentsList"]),
     total() {
       return this.$store.getters.getPaymentsListFullValuePrice;
+    },
+    currentElement() {
+      return this.paymentsList.slice(
+        this.n * (this.page - 1),
+        this.n * (this.page - 1) + this.n
+      );
     },
   },
   methods: {
     changePage(p) {
       this.page = p;
+      this.fetchData(p);
+    },
+    openModal() {
+      this.$modal.show("AddPaymentForm", {
+        content: "addPayment",
+        header: "Add new cost",
+      });
     },
     ...mapActions(["fetchData"]),
     ...mapMutations({ fetch: "setPaymentsListData" }),
-    fetchData() {
-      return [
-        {
-          date: "20.12.2021",
-          category: "Food",
-          value: 169,
-        },
-        {
-          date: "20.12.2021",
-          category: "Transport",
-          value: 360,
-        },
-        {
-          date: "20.12.2021",
-          category: "Food",
-          value: 532,
-        },
-      ];
-    },
-  },
-  addToPaymentList(props) {
-    console.log("run");
-    this.paymentsList = [...this.paymentsList, props];
   },
   async created() {
-    await this.fetchData();
+    await this.fetchData(1);
     if (this.$route.params?.page) {
       this.page = Number(this.$route.params.page);
     }
-    //this.$store.commit('setPaymentsListData', this.fetchData());
-    //this.fetch(this.fetchData());
-    // this.$store.dispatch('fetchData')
   },
 };
 </script>
