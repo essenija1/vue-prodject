51446d35c9050488d592f873af835dec6905861a
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-09 22:56:44 +0300

    Create TestVuex.vue

---
+++ tests/TestVuex.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+       <input v-model="propComp" name="test" />
+    <button @click="onSave" name="save">Save</button>
+    </div>
+</template>
+
+<script>
+import {mapActions, mapMutations} from 'vuex'
+export default {
+    name: "TestVuex",
+    data () {
+        return {
+            propComp: ''
+        }
+    },
+    methods: {
+        ...mapMutations(['addData']),
+        onSave() {
+            this.addData(this.propComp)
+        }
+    }
+
+}
+
+</script>
+<style></style>
