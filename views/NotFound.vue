2969e4bbe4194418c4dd5c49af1cb2c43b6ccae3
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-09 22:52:06 +0300

    Update NotFound.vue

--- views/NotFound.vue
+++ views/NotFound.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>notfound</div>
+  <div>
+    not found
+    <calculator />
+   <!--<router-link
+    :to="{
+       name: 'addPayment',
+        params: {
+        category: 'Education',
+       },
+      query: { value: 200 },
+      }"
+     >Add Education</router-link>-->
+  </div>
 </template>
 
 <script>
-    export default{
-
-}
+import Calculator from '../components/Calculator.vue'
+export default {
+  components: { Calculator }
+};
 </script>
 <style>
-
 </style>
