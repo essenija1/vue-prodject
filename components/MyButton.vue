1f280f8562ef7003dece6995f54b168769db6628
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-13 13:07:35 +0300

    Update MyButton.vue

--- components/MyButton.vue
+++ components/MyButton.vue
@@ -1,55 +1,66 @@
 <template>
-<button @click="onClick">Clicked {{ counter }} times!</button>
+  <div>
+    <div class="icon">
+      <slot name="icon" />
+    </div>
+    <button><slot name="default" v-bind:props="upperCaseText">Жми</slot></button>
+  </div>
 </template>
 <script>
 export default {
-data () {
-return {
-counter: 0,
-}
-},
-methods: {
-onClick () {
-this.counter++
-},
-mouseOver(){
-     console.log('Hovered on button')
-}
-},
-beforeCreate () {
-console.log('beforeCreate')
-},
-created() {
-    console.log('created')
-},
-beforeMount() {
-  console.log('beforeMount')
-},
-mounted() {
-   console.log('mounted')
-   const btn = this.$el.querySelector('button')
-   if (btn) {
-   btn.addEventListener('mouseover', this.onMouseOver)
-}
-},
-beforeUpdate() {
-     console.log('beforeUpdate');
-},
-update() {
-    console.log('updated')
-},
+  props: ["text"],
+  data() {
+    return {};
+  },
+  methods: {
+    //onClick() {
+    //this.$emit("click");
+    // },
+  },
+  computed: {
+      upperCaseText() {
+        return "1234"
+      }
+  },
+  //beforeCreate () {
+  //console.log('beforeCreate')
+  //},
+  //created() {
+  //  console.log('created')
+  //},
+  //beforeMount() {
+  //console.log('beforeMount')
+  //},
+  //mounted() {
+  //  console.log('mounted')
+  //   const btn = this.$el.querySelector('button')
+  //   if (btn) {
+  //  btn.addEventListener('mouseover', this.onMouseOver)
+  //}
+  //},
+  //beforeUpdate() {
+  //    console.log('beforeUpdate');
+  ///},
+  //update() {
+  //    console.log('updated')
+  //},
 
-beforeDestroy()
-{
-   const btn = this.$el.querySelector('button')
-   if (btn) {
-   btn.removeEventListener('mouseover', this.onMouseOver)
-   }
-},
+  //beforeDestroy()
+  //{
+  //  const btn = this.$el.querySelector('button')
+  //  if (btn) {
+  //  btn.removeEventListener('mouseover', this.onMouseOver)
+  //  }
+  //},
 
-destroyed()
-{
-console.log('destroyed')
-},
+  //destroyed()
+  //{
+  //console.log('destroyed')
+  //},
 };
 </script>
+<style scoped>
+.btn{
+  display: flex;
+}
+</style>
