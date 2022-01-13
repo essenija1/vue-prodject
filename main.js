e33a005475e775297f7332a4a49ff8c2e8b4cbae
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-13 13:15:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import modal from './plugins/ModalWindow'
+import modal from './plugins/ModalWindow';
+//import context from './plugins/ContextMenu';
+import vuetify from './plugins/vuetify';
+
 
 
 
 Vue.config.productionTip = false;
-Vue.use(modal)
+Vue.use(modal);
+//Vue.use(context);
 
 new Vue({
+  render: h => h(App),
   router,
   store,
-  modal,
-  render: h => h(App)
+  //modal,
+  vuetify
+  
 }).$mount('#app')
