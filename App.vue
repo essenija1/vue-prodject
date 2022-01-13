f988c75f9f37672f9f199addf0fe9fe4ab99b5de
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-13 13:13:45 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,168 +1,35 @@
 <template>
-  <div id="app">
-    <div class="wrapper">
-      <header>
-        <router-link to="/dashboard">dashboard</router-link> /
-        <router-link to="/about">about</router-link> /
-        <span @click="goToPageNotFound">notfound</span>
-      </header>
-      <main>
-        <router-view />
-        <calculator />
-      </main>
-      <transition name="fade">
-      <modal-window-add-payment-form 
-      :settings="setting" 
-      :componentName = "componentName"
-      v-if="componentName" />
-      </transition>
-    </div>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      flat
+    >
+    <v-btn plain :ripple="false" to="/dashboard">Dashboard</v-btn>
+
+    <v-btn plain :ripple="false" to="/about">About</v-btn>
+
+    <v-btn plain :ripple="false" to="/notfound">Not Found</v-btn>
+
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-//import ModalWindowAddPaymentForm from './components/ModalWindowAddPaymentForm.vue';
+import {mapActions} from 'vuex'
 export default {
-  components: { ModalWindowAddPaymentForm: () =>import(/*webpackChunkName: "Modal"*/'./components/ModalWindowAddPaymentForm.vue') },
-  name: "App",
-  data() {
-    return {
-      modalShow: false,
-      setting: {},
-      componentName: ''
-    };
-  },
+  name: 'App',
+
+  data: () => ({
+  
+  }),
   methods: {
-    ...mapActions(["fetchData"]),
-    goToPageNotFound() {
-      if(this.$route.name === 'notfound') return
-      this.$router.push({
-        name:"notfound"
-      });
-    },
-    onShow({name, setting}) {
-        this.componentName = name
-        this.settings = settings
-    },
-    onHide() {
-      this.setting = {}
-        this.componentName = ''
-    },
+      ...mapActions(["fetchData"]),
   },
   async created() {
       await this.fetchData();
   },
-
-  mounted () {
-    this.$modal.Eventbus.$on('show')
-    this.$modal.Eventbus.$on('hide')
-  },
-beforeDestroy () {
-    this.$modal.Eventbus.$off('show')
-    this.$modal.Eventbus.$off('hide')
-  },
-
-  created() {
-    this.$modal.show()
-    this.$modal.hide()
-  },
 };
-  //methods: {
-    //setPage() {
-    //  this.page = location.pathname.slice(1);
- //   },
- // },
- // mounted() {
-  //  this.setPage();
-  //  const links = document.querySelectorAll('a');
-   // links.forEach(links=>{
-   //    link.addEventListener('click', event=>{
-    //     event.preventDefault();
-     //    history.pushState({}, '', link.href);
-     //    this.setPage();
-   //    });
-   // });
-  //  this.setPage();
-  //  window.addEventListener("popstate", () => {
-  //    this.setPage();
- // //  });
- // },
-//};
-//computed: {
-// ...mapGetters({paymentsList: 'getPaymentsList'}),
-// total(){
-//   return this.$store.getters.getPaymentsListFullValuePrice
-// }
-//},
-//methods: {
-// changePage(p) {
-//   this.page = p
-// },
-// ...mapActions(['fetchData']),
-// ...mapMutations({fetch: "setPaymentsListData"}),
-// fetchData () {
-//return [
-//{
-//date: '20.12.2021',
-//category: 'Food',
-//value: 169,
-//},
-//{
-//date: '20.12.2021',
-//category: 'Transport',
-//value: 360,
-//},
-//{
-//date: '20.12.2021',
-//category: 'Food',
-//value: 532,
-//},
-//];
-//},
-//},
-//addToPaymentList(props) {
-//    console.log('run')
-//  this.paymentsList = [...this.paymentsList, props]
-//},
-//created() {
-//    this.fetchData();
-//this.$store.commit('setPaymentsListData', this.fetchData());
-//this.fetch(this.fetchData());
-// this.$store.dispatch('fetchData')
-//}
-//};
-
 </script>
-
-<style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-  h1 {
-    font-size: 20px;
-  }
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .3s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
-}
-</style>
