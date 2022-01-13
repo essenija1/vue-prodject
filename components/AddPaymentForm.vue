cb11611dc1c6ca3a03cc53023dcc0ba771c91234
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-13 13:06:35 +0300

    Update AddPaymentForm.vue

--- components/AddPaymentForm.vue
+++ components/AddPaymentForm.vue
@@ -1,61 +1,58 @@
 <template>
-    <div class="wrapper">
-       <input placeholder="date" v-model="date"/>
-       <div class="categoryList">
-           <select v-model="category">
-            <option v-for="(category, idx) in categoryList" :key="idx" :value="category">{{ category}}
-
-            </option>
-           </select>
-       </div>
-       <input placeholder="value" type="number" v-model="value"/>
-       <button @click="onClick">Save!</button>
-    </div>
+  <v-card class="text-left pa-8">
+    <v-text-field v-model="date" label="Date" />
+    <v-select
+          :items="categoryList"
+          label="Category"
+          v-model="category"
+        ></v-select>
+    <v-text-field v-model.number="value" label="Value" />
+    <v-btn @click="onClick">Save!</v-btn>
+  </v-card>
 </template>
+
 <script>
-
-import { mapMutations, mapActions, mapGetters } from 'vuex'
+import { mapMutations, mapActions, mapGetters } from "vuex";
 export default {
-    name: "AddPaymentForm",
-       data() {
-           return {
-               date:"",
-               category: "",
-               value: "",
-           };
-       },
-       computed: {
-           ...mapGetters({CategoryList:'getCategoryList'}),
-            getCurrentDate() {
-                const today = new Date() 
-                const d = today.getDate()
-                const m = today.getMonth() +1
-                const y = today.getFullYear()
-                return `${d}.${m}.${y}`
-            }
-       },
-       methods: {
-           ...mapActions(['fetchCategory']),
-           ...mapMutations(['addPaymentListData']),
-            onclick() {
-                const data = {
-                    date: this.date || this.getCurrentDate,
-                    category: this.category,
-                    value: this.value
-
-                };
-                this.addPaymentListData(data);
-               this.$emit('addNewPayment', data);
-            },
-       },
-      async mounted() {
-           if(!this.getCategoryList?.length) {
-             await this.fetchCategory();
-             this.category = this.CategoryList[0]
-           }
-       }
+  name: "AddPaymentForm",
+  data() {
+    return {
+      date: "",
+      category: "",
+      value: "",
+    };
+  },
+  computed: {
+    ...mapGetters({ categoryList: "getCategoryList" }),
+    getCurrentDate() {
+      const today = new Date();
+      const d = today.getDate();
+      const m = today.getMonth() + 1;
+      const y = today.getFullYear();
+      return `${d}.${m}.${y}`;
+    },
+  },
+  methods: {
+    ...mapMutations(["addPaymentListData"]),
+    ...mapActions(["fetchCategory"]),
+    onClick() {
+      const data = {
+        date: this.date || this.getCurrentDate,
+        category: this.category,
+        value: this.value,
+      };
+      this.addPaymentListData(data);
+      this.$emit("addNewPayment", data);
+    },
+  },
+  async mounted() {
+    await this.fetchCategory();
+    if(this.$route.params && this.$route.params.category) {
+      this.category = this.$route.params.category
+    }
+    if(this.$route.query?.value){
+      this.value = Number(this.$route.query.value)
+    }
+  },
 };
 </script>
-<style>
-
-</style>
