0d84ff89f6f7c2b879d5b170db519556009f9206
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-18 17:57:34 +0300

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,70 +1,75 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/add/payment/:category",
-    //path: "/:action/:section/:category",
     name: "addPayment",
-    component: ()=>import('../components/AddPaymentForm.vue')
+    component: () =>
+      import('../components/AddPaymentForm.vue')
   },
   {
     path: "/dashboard",
     name: "dashboard",
-    component: () => import(/*webpackChunkName: "Dashboard" */'../views/Dashboard.vue'),
-    
+    component: () =>
+      import(/* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
   },
   {
-  path: "/dashboard/:page",
-  name: "dashboard",
-  component: () => import(/*webpackChunkName: "Dashboard" */'../views/Dashboard.vue'),
+    path: "/dashboard/:page",
+    name: "dashboard",
+    component: () =>
+      import(/* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
   },
-{
-  path: "/about",
+  {
+    path: "/about",
     name: "about",
-    component: () => import(/*webpackChunkName: "About" */'../views/About.vue'),
+    component: () =>
+      import(/* webpackChunkName: "About" */ "../views/About.vue"),
   },
-{
-  path: '/notfound',
-    name: 'notfoundt',
-    component: () => import(/*webpackChunkName: "NotFound" */'../views/NotFound.vue'),
+  {
+    path: "/notfound",
+    name: "notfound",
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
   },
-{
-  path: "*",
-  component: () => import(/*webpackChunkName: "NotFound" */'../views/NotFound.vue'),
+  {
+    path: "*",
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  mode: "history",
+  routes,
 });
 
 const userAuth = true;
 
-
 router.beforeEach((to, from, next) => {
-  if (to.name !== 'notfound' && !userAuth) {
+  if (to.name !== "notfound" && !userAuth) {
     next({
-      name: "notfound"
+      name: "notfound",
     });
   } else {
     next();
   }
 });
-const getTitleByRoutes = routeName => {
-  return{
-    'dashboard': "Ура, у нас ест страница наших трат",
-    'about': "Страница о сервисе",
-    'notfound': "Страница не найдена"
+
+const getTitleByRoutes = (routeName) => {
+  return {
+    dashboard: "Ура, у нас есть страница наших трат",
+    about: "Страница о сервисе",
+    notfound: "Страница не найдена",
   }[routeName];
 };
-router.afterEach((to)=>{
-  document.title = getTitleByRoutes(to.name)
+
+router.afterEach((to) => {
+  document.title = getTitleByRoutes(to.name);
 });
+
+
+
+
 export default router;
