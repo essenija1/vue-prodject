de67fb5bac03ddb094397ba586c616f567047ce8
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-18 22:02:17 +0300

    Update Dashboard.vue

--- views/Dashboard.vue
+++ views/Dashboard.vue
@@ -4,21 +4,23 @@
       <v-col>
         <div class="text-h5 text-sm-h3 mb-8">My personal cost</div>
         <v-dialog v-model="dialog" width="500">
-          <template v-slot:activator="{on}">
+          <template v-slot:activator="{ on }">
             <v-btn color="teal" dark v-on="on">
               ADD NEW COST <v-icon>mdi-plus</v-icon>
             </v-btn>
+            <div class="app">
+              {{ message }}
+              <line-chart></line-chart>
+            </div>
           </template>
-     
-            <add-payment-form @addNewPayment="dialog=false"/>
-          
+
+          <add-payment-form @addNewPayment="dialog = false" />
         </v-dialog>
         <!--<my-button>
           <template #icon>
             <v-icon>mdi-plus</v-icon>
             </template>Жми</my-button>
             <my-button><template #default="props">{{props}}</template></my-button>-->
-        
 
         <payments-display :items="currentElement" />
         <!--<pagination 
@@ -34,9 +36,11 @@
 </template>
 
 <script>
+import ModalButton from "../components/ModalButton.vue";
 import PaymentsDisplay from "../components/PaymentsDisplay.vue";
 import { mapMutations, mapGetters, mapActions } from "vuex";
-import AddPaymentForm from '../components/AddPaymentForm.vue';
+import AddPaymentForm from "../components/AddPaymentForm.vue";
+//import Chart from '../components/Chart.vue'
 
 //import Pagination from "../components/Pagination.vue";
 //import MyButton from "../components/MyButton.vue";
@@ -48,6 +52,7 @@ export default {
     AddPaymentForm,
     //Pagination,
     //MyButton
+    ModalButton
   },
   data() {
     return {
@@ -68,10 +73,7 @@ export default {
     },
     currentElement() {
       const { n, page } = this;
-      return this.paymentsList.slice(
-        n * (page - 1),
-        n * (page - 1) + n);
-    
+      return this.paymentsList.slice(n * (page - 1), n * (page - 1) + n);
     },
   },
   methods: {
@@ -87,18 +89,16 @@ export default {
       });
     },
   },
-  
+
   async created() {
-     // if(!paymentsList.lenght) {
-     // await this.fetchData(1);
+    // if(!paymentsList.lenght) {
+    // await this.fetchData(1);
     // }
     if (this.$route.params?.page) {
       this.page = Number(this.$route.params.page);
     }
   },
-  
 };
-
 </script>
 <style>
 </style>
