be3d2c4fa06a548faa1799f4c0da6a10ac307a02
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-18 18:02:01 +0300

    Create ApexChart.vue

---
+++ components/ApexChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <apexchart
+      width="500"
+      type="bar"
+      :options="options"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+   <script>
+export default {
+  data() {
+    return {
+      componentKey: 0,
+    };
+  },
+  methods: {
+    forceRerender() {
+      this.componentKey += 1;
+    },
+  },
+};
+</script>
